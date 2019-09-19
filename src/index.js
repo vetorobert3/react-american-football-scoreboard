754c1513dd936f4d0710ca6edae1355bc6913a19
Veto Ramirez <vetorobert3@gmail.com>
2019-09-18 18:57:59 -0700

    added touchdown and field goal scores for home team.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function App() {
-
-  const []
-
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
